0e537a746d48b44c93bd7df9b1e2ea8a58625cee
Matus Horvath <matus.horvath@gmail.com>
2022-12-04 18:47:32 +0100

    Add plugin link to each board

--- test/board-format.test.js
+++ test/board-format.test.js
@@ -15,49 +15,63 @@ describe('formatBoard', () => {
 
     test('day 1: person one part1, person two part2', async () => {
         expect(formatBoard(2021, 1, jsonLeaderboard)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person Two 95:59:59    1234d \\(   1230d\\)
-      Person One 00:00:03 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+      Person One 00:00:03 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 2: person one part2, person two part1', async () => {
         expect(formatBoard(2021, 2, jsonLeaderboard)).toEqual(`\
-Day  2 @ 11d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  2 @ 11d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person One 00:07:42 00:31:16 \\(00:23:34\\)
-      Person Two  13d  7h \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+      Person Two  13d  7h \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 3: both part2', async () => {
         expect(formatBoard(2021, 3, jsonLeaderboard)).toEqual(`\
-Day  3 @ 10d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  3 @ 10d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person Two 03:12:34 12:54:32 \\(09:41:58\\)
-      Person One 234d 12h 234d 12h \\(00:00:01\\)`);
+      Person One 234d 12h 234d 12h \\(00:00:01\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 4: only person one, part 2', async () => {
         expect(formatBoard(2021, 4, jsonLeaderboard)).toEqual(`\
-Day  4 @  9d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-      Person One 25:00:13   4d  4h \\(75:36:34\\)`);
+Deň  4 @  9d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+      Person One 25:00:13   4d  4h \\(75:36:34\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 5: only person two, part 1', async () => {
         expect(formatBoard(2021, 5, jsonLeaderboard)).toEqual(`\
-Day  5 @  8d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-      Person Two 01:08:09 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+Deň  5 @  8d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+      Person Two 01:08:09 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 6: equal times both parts', async () => {
         expect(formatBoard(2021, 6, jsonLeaderboard)).toEqual(`\
-Day  6 @  7d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  6 @  7d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person One 00:07:42 00:31:16 \\(00:23:34\\)
-      Person Two 00:07:42 00:31:16 \\(00:23:34\\)`);
+      Person Two 00:07:42 00:31:16 \\(00:23:34\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('day 7: equal times part 1', async () => {
         expect(formatBoard(2021, 11, jsonLeaderboard)).toEqual(`\
-Day 11 @55:00:00 ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň 11 @55:00:00 ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person Two 00:07:42 00:31:16 \\(00:23:34\\)
-      Person One 00:07:43 00:31:16 \\(00:23:33\\)`);
+      Person One 00:07:43 00:31:16 \\(00:23:33\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('ordering of missing ts2', async () => {
@@ -84,9 +98,11 @@ Day 11 @55:00:00 ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         };
 
         expect(formatBoard(2021, 1, leaderboard)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person Two 05:27:49 05:37:20 \\(00:09:31\\)
-      Person One 05:27:49 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+      Person One 05:27:49 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('adds results from startTimes', async () => {
@@ -112,10 +128,12 @@ Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         };
 
         expect(formatBoard(2021, 5, leaderboard, startTimes)).toEqual(`\
-Day  5 @  8d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
+Deň  5 @  8d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
       Person One 01:08:20 \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\) \\[00:16:40 \\-\\-:\\-\\-:\\-\\-\\]
  Still Working 1 \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\) \\[\\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\-\\]
- Still Working 2 \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\) \\[\\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)\\]`);
+ Still Working 2 \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\) \\[\\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)\\]
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('displays infinity for dates far back', async () => {
@@ -125,8 +143,10 @@ Day  5 @  8d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         } } };
 
         expect(formatBoard(2021, 25, leaderboard)).toEqual(`\
-Day 25 @\\-\\-:\\-\\-:\\-\\- ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-  Connor MacLeod        ∞ \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+Deň 25 @\\-\\-:\\-\\-:\\-\\- ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+  Connor MacLeod        ∞ \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('handles missing get_star_ts', async () => {
@@ -138,8 +158,10 @@ Day 25 @\\-\\-:\\-\\-:\\-\\- ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(
 
         // If the person is in the input but with no time, add them to leaderboard with no time
         expect(formatBoard(2021, 1, leaderboard)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-      Person One \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+      Person One \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('ignores negative solution times', async () => {
@@ -151,8 +173,10 @@ Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         const startTimes = { '2021': { '1': { 'Person One': { '1': [1639526400], '2': [1639530000] } } } };
 
         expect(formatBoard(2021, 1, leaderboard, startTimes)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-      Person One  12d 19h  12d 20h \\(01:00:00\\)`);
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+      Person One  12d 19h  12d 20h \\(01:00:00\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('ignores negative difference between part 1 and part 2', async () => {
@@ -165,8 +189,10 @@ Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         const startTimes = { '2021': { '1': { 'Person One': { '1': [1639429200], '2': [1639441800] } } } };
 
         expect(formatBoard(2021, 1, leaderboard, startTimes)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-      Person One  12d 20h  12d 19h \\(\\-\\-:\\-\\-:\\-\\-\\) \\[04:00:00 03:00:00\\]`);
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+      Person One  12d 20h  12d 19h \\(\\-\\-:\\-\\-:\\-\\-\\) \\[04:00:00 03:00:00\\]
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     test('escapes backslash correctly', async () => {
@@ -176,8 +202,10 @@ Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na
         } } };
 
         expect(formatBoard(2021, 1, leaderboard)).toEqual(`\
-Day  1 @ 12d  7h ofic\\. part 1 a 2 \\(cas na p2\\)  neoficialne \\(cisty cas na p2\\)
-     Per\\\\son One \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)`);
+Deň  1 @ 12d  7h ofic\\. part 1 a 2 \\(čas na p2\\) neoficiálne \\(čistý čas na p2\\)\\*
+     Per\\\\son One \\-\\-:\\-\\-:\\-\\- \\-\\-:\\-\\-:\\-\\- \\(\\-\\-:\\-\\-:\\-\\-\\)
+
+\\* čistý čas zistený pluginom [https://github.com/TrePe0/aoc-plugin](https://github.com/TrePe0/aoc-plugin)`);
     });
 
     // TODO start times that exist but don't include current year
