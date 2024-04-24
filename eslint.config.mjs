9cd22d641ecc5f2eaba8a1b0407029192004c798
Matus Horvath <matus.horvath@gmail.com>
2024-04-24 16:53:48 +0200

    Migrate eslint to flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import jest from 'eslint-plugin-jest';
+import globals from 'globals';
+
+export default [
+    js.configs.recommended,
+    jest.configs['flat/recommended'],
+
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            sourceType: 'commonjs'
+        }
+    },
+
+    {
+        languageOptions: {
+            ecmaVersion: 2024,
+            globals: { ...globals.node, ...globals.commonjs, ...globals.es2024 }
+        },
+
+        rules: {
+            'comma-dangle': 'error',
+            'indent': ['error', 4],
+            'max-len': ['error', 120, { 'ignoreStrings': true, 'ignoreTemplateLiterals': true }],
+            'no-multi-spaces': ['error', { 'ignoreEOLComments': true }],
+            'no-multiple-empty-lines': 'error',
+            'no-shadow': 'error',
+            'no-unused-vars': ['error', { 'varsIgnorePattern': '^_', 'argsIgnorePattern': '^_' }],
+            'object-curly-spacing': ['error', 'always'],
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
+            'semi': 'error',
+            'strict': 'error'
+        }
+    }
+];
